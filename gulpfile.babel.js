e1d504101e61720e3e06f035bdc74c17481a6371
Ana Maria Mihalceanu <mihalceanuana@gmail.com>
2022-03-21 16:37:45 +0100

    Update gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,7 +7,7 @@ import {load as yamlLoad} from "yaml-js";
 import generator from "@antora/site-generator-default";
 import browserSync from "browser-sync";
 
-const filename = "github-pages.yml";
+const filename = "dev-site.yml";
 const server = browserSync.create();
 const args = ["--playbook", filename];
 
@@ -16,11 +16,11 @@ function watchGlobs() {
   let json_content = readFileSync(`${__dirname}/${filename}`, "UTF-8");
   let yaml_content = yamlLoad(json_content);
   let dirs = yaml_content.content.sources.map(source => [
-    `**/*.yml`,
-    `**/*.adoc`,
-    `**/*.hbs`
-  ]);
-  dirs.push(["${filename}"]);
+    `${source.url}/**/**.yml`,
+    `${source.url}/**/**.adoc`,
+    `${source.url}/**/**.hbs`
+  ]); 
+  dirs.push(["dev-site.yml"]);
   dirs = [].concat(...dirs);
   //console.log(dirs);
   return dirs;
